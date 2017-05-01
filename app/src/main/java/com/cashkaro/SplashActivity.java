38cead620df69a6c99ef2292bbcf92a18f263644
Rohan Garg <dev.RohanGarg@gmail.com>
2017-05-01 19:53:45 +0530

    feat: Add activity transition animations

--- app/src/main/java/com/cashkaro/SplashActivity.java
+++ app/src/main/java/com/cashkaro/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.cashkaro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 
@@ -13,5 +15,14 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         Utils.setFontAllView((ViewGroup) findViewById(R.id.root));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                overridePendingTransition(R.anim.push_right_in, R.anim.push_left_out);
+                finish();
+            }
+        }, 1500);
     }
 }
